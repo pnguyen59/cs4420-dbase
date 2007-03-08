41deb4cb8a7db51f702dd7cc529adb4aacdcb634
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-03-08 18:33:36 +0000

    More javadoc.  Just trying to specify what it all needs to do.

--- trunk/cs4420-dbase/dbase/StorageManager.java
+++ trunk/cs4420-dbase/dbase/StorageManager.java
@@ -10,8 +10,7 @@
 
 package dbase;
 
-import java.util.*;
-import java.nio.*;
+import java.nio.MappedByteBuffer;
 
 /**
  *
@@ -20,87 +19,70 @@ import java.nio.*;
 public class StorageManager {
     
     
-    /**
-     *The System Catalog that owns the buffermanager this belongs to, used to allow it to get relation files and others.
-     *
-     **/
+    /**The System Catalog that owns the buffermanager this belongs to, 
+     * used to allow it to get relation files and others.*/
     private SystemCatalog catalog;
     
     
-    /** Creates a new instance of StorageManager */
+    /** Creates a new instance of StorageManager. */
     public StorageManager() {
         
     }
     
-    /**
-     *This scans an index and returns the blocks.
-     *
-     *@param index The Index ID to load.
-     *
-     *@return the first block of the index.
-     **/
-    public MappedByteBuffer indexScan(int index) {
+    /**This scans an index and returns the first block from the index.
+     * @param relation The relation for which the index is being found.
+     * @param index The Index ID to load.
+     * @return The first block of the index as a MappedByteBuffer.
+     */
+    public MappedByteBuffer indexScan(final int relation, final int index) {
         return null;
     }
     
-    /**
-     *This creates a new file for whatever the hell you want.
-     *
-     *@param file the filename
-     *
-     *@return whether or not the file was sucessfully created.
+    /**This creates a new file wiht the given name.
+     * @param fileName The name of the new file to be created.
+     * @return Whether or not the file was sucessfully created.
      */
-    public boolean makeFile(String file) {
+    public boolean makeFile(final String fileName) {
         return true;
     }
     
-    /**
-     *This opens a file for the storage manager to read into or write out of.
-     *
-     *@param file the filename to open
-     *
-     *@return the opened file stream (implement)
+    /**This opens a file for the storage manager to read into or write out of.
+     * @param fileName The name of the file to open.
+     * @return The FileChannel mapped to the newly opened file, or null if it
+     * could not be opened.
      */
-    public boolean openFile(String file) {
+    public boolean openFile(final String fileName) {
         return true;
     }
     
     
-    /**
-     *This reads the requested block from the requested relation's block and returns it.
-     *
-     *@param relation the relation to read.
-     *@param block the block needed from that relation.
-     *
-     *@return the MappedByteBuffer of the block specified or null if read fails.
+    /**This reads the requested block from the requested relation's file
+     * and returns it.
+     * @param relation the relation to read.
+     * @param block the block needed from that relation.
+     * @return The MappedByteBuffer of the block specified or null if read 
+     * fails.
      */
-    public MappedByteBuffer read (int relation, long block) {
+    public MappedByteBuffer read(final int relation, final long block) {
         return null;
     }
     
-    /**
-     *Reads in the first block of the table.
-     *
-     *@param table the ID of the relation to scan.
-     *
-     *@return the first block.
+    /**Reads in the first block of the specified relation.
+     * @param relation The ID of the relation to scan.
+     * @return The first block of the relation as a MappedByteBuffer.
      */
-    
-    public MappedByteBuffer tableScan(int table) {
+    public MappedByteBuffer tableScan(final int relation) {
         return null;
     }
     
-    /**
-     *This writes the requested block to the requested relation's block and writes it.
-     *
-     *@param relation the relation to read.
-     *@param address the block needed from that relation.
-     *@param block the information to be written
-     *
-     *@return whether or not the write succeeded.
-     */
-    
-    public boolean write(int relation, long address, MappedByteBuffer block) {
+    /**This writes the given block to the block in the relation specified.
+     *@param relation The relation to write the block to.
+     *@param address The block to write to.
+     *@param block The information to be written to disk.
+     *@return Whether or not the write succeeded.
+     */  
+    public boolean write(final int relation, final long address, 
+    		final MappedByteBuffer block) {
         return true;
     }
 }
