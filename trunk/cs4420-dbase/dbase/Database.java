6bd0f9569bb022e31d060b0559d8aa37696b9a3c
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-03-08 15:36:16 +0000

    Tried to make more clear what things will do.

--- trunk/cs4420-dbase/dbase/Database.java
+++ trunk/cs4420-dbase/dbase/Database.java
@@ -15,14 +15,20 @@ package dbase;
  */
 public class Database {
     
-    /** Creates a new instance of Database */
+    /** Creates a new instance of Database. */
     public Database() {
     }
     
+    /**This will create a new database if one isn't specified by the user.
+     */
     public void initialize() {
         
     }
     
+    /**This will look at a command to see if it is valid.  This will be moved
+     * to QueryParser in phase 2 of the project.
+     * @return Whether or not the query could be parsed correctly.
+     */
     public boolean parseCommand() {
         return true;
     }
