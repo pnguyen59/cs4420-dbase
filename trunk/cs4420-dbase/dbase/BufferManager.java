8a7a1e5056673ea75f5558df0a5ca1c47ef0336e
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-03-08 15:35:49 +0000

    I replaced the one long for the address of the block with an int for the relation and a long for the block.  It's so much simpler than getting the relation and the block out of the one long.  Also, this is revenge for confusing the hell out of me and saying it would have the "RAID ID" in it when you meant "Relation ID."

--- trunk/cs4420-dbase/dbase/BufferManager.java
+++ trunk/cs4420-dbase/dbase/BufferManager.java
@@ -9,23 +9,23 @@
 
 package dbase;
 
-import java.nio.*;
+import java.nio.MappedByteBuffer;
+
 /**
- *
  * @author andrewco
  */
 public class BufferManager {
     
-    /** The Buffer Implenetation. */
+    /**The Buffer Implenetation.*/
     private MappedByteBuffer [] buffer;
     
-    /** A lookup table for the buffer */
+    /**A lookup table for the buffer.*/
     private long [][] lookUpTable;
     
-    /** The current position of the clock */
+    /**The current position of the clock.*/
     private long time = 0;
     
-    /** Creates a new instance of BufferManager */
+    /**Creates a new instance of BufferManager.*/
     public BufferManager() {
     }
     
@@ -39,58 +39,59 @@ public class BufferManager {
     }
     
     
-    /**
-     *Finds a spot to free up for a new block in memory and returns it's location.
-     *
-     *@return The freed buffer block.
+    /**Finds a spot to free up for a new block in memory and return it's
+     *logical address.
+     *@return The logical address of the freed block.
      */
     private long freeSpace() {
         return time; 
     }
     
     
-    /**
-     *Pins a block into memory.
-     *
-     *@param address the physical address of the block to pin into memory
-     *
-     *@return whether the block was successfully pinned.
-     */
-            
-    public boolean pin(long address) {
+    /**Pins a block currently in memory.
+     *@param relation The relation containing the block to be pinned.
+     *@param block  The block in the relation to be pinned.
+     *@return Whether or not the block was successfully pinned.
+     */    
+    public boolean pin(final int relation, final long block) {
         return true;
     }
     
-    /**
-     *Reads a block from memory, will load block into memory if not present.
-     *
-     *@param address The physical address of the block wanted
-     *
-     *@return the block requested.
+    /**Reads a block from memory, will load block into memory if not already in
+     *the buffer.
+     *@param relation The relation that the requested block is in.
+     *@param address The block from the specified relation.
+     *@return The block requested in a MappedByteBuffer.
      */
-    public MappedByteBuffer read(long address) {
+    public MappedByteBuffer read(final int relation, final long address) {
         return null;
     }
     
-    /**
-     *Removes the pin from a block in memory.
-     *
-     *@param address The physical address of the block to be unpinned.
-     *
-     *@return whether the block was successfully unpinned.
+    /**Removes the pin from a block in memory.
+     *@param relation The ID of the relation containing the block to be 
+     *unpinned.
+     *@param block The block in the relation to be unpinned.
+     *@return Whether or not the block was successfully unpinned.
      */
-    public boolean unpin(long address) {
+    public boolean unpin(final int relation, final long block) {
         return true;
     }
     
-    /**
-     *Writes a particular block to disk.
-     *
+    /***Writes the specified block to disk.
      *@param address The <b>logical</b> address to be written to disk.
-     *
      *@return whether the block was successfully written.
      */
-    public boolean write(long address) {
+    public boolean write(final long address) {
         return true;
+    }
+    
+    /**This method will return whether or not the specified block is pinned in
+     * memory.
+     * @param relation The relation containing the block.
+     * @param block The block inside of that relation.
+     * @return Whether or not the block is pinned.
+     */
+    public boolean isPinned(final int relation, final long block) {
+    	return false;
     }
 }
