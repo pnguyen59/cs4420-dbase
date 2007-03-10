cfad4acd004f350c379ac454a9b19c5c18ba5405
killer.ego <killer.ego@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-03-10 18:20:55 +0000

    Added code to simply make a file if it doesn't exist when the program attempts to open it. Added a singleton class for relation and RelationHolder.  This will allow our program to more easily access filenames from Relation ID's.

---
+++ trunk/cs4420-dbase/dbase/Relation.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package dbase;
+
+/**
+ * @author gtg471h
+ * 
+ * This Class to be used as a wrapper class for all the Relation 
+ * information held in the database.  1 instance per Relation.
+ */
+public class Relation {
+	
+	public Relation() {
+		
+	}
+
+}
