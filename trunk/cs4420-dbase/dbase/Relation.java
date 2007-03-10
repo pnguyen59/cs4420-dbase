ca0271379c5a8e6fe567c01523319b04e4bd63d0
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-03-10 20:35:14 +0000

    Implemented the open method.  Added javadoc.

--- trunk/cs4420-dbase/dbase/Relation.java
+++ trunk/cs4420-dbase/dbase/Relation.java
@@ -10,35 +10,56 @@ package dbase;
  * information held in the database.  1 instance per Relation.
  */
 
-import java.nio.channels.*;
+import java.nio.channels.FileChannel;
 
 public class Relation {
 	
+	/**The name of the file which holds this relations records.*/
 	private final String filename;
+	
+	/**The internal, autmatically assigned ID of this relation.*/
 	private final int ID;
+	
+	/**The FileChannel which maps to this relation and reads from it.*/
 	private FileChannel channel;
 	
-	public Relation(String filename, int ID) {
-		this.filename = filename;
-		this.ID = ID;
+	/**This creates a new instance of relation.
+	 * @param newfilename The file that holds the records of this relation.
+	 * @param newID The unique internal ID of this relation.
+	 */
+	public Relation(final String newfilename, final int newID) {
+		this.filename = newfilename;
+		this.ID = newID;
 		channel = StorageManager.openFile(filename);
 	}
 
+	/**Returns the name of the file that holds the records of this relation.
+	 * @return The name of this relations file.
+	 */
 	public String getFilename() {
 		return filename;
 	}
 
+	/**Returns the internal ID of this relation.
+	 * @return The ID of this relation.
+	 */
 	public int getID() {
 		return ID;
 	}
 
+	/**Returns the FileChannel mapped to this relations file.
+	 * @return The FileChannel of this relation.
+	 */
 	public FileChannel getChannel() {
 		return channel;
 	}
 	
-//	public Iterator Open() {
-//	TODO implement once Iterator is uploaded.	
-//	}
+	/**This method opens up an Iterator for this relation.
+	 * @return An instance of Iterator for this relation.
+	 */
+	public Iterator open() {
+		return new Iterator(this);	
+	}
 	
 	public void Close() {
 		//TODO Closes the iterator thingy.
