8bdfea380d3660f2e451f6dd7366ecabeb2508d3
danielkitchener1 <danielkitchener1@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-03-16 03:15:01 +0000

    made it parse some stuff. die.

--- trunk/cs4420-dbase/dbase/Database.java
+++ trunk/cs4420-dbase/dbase/Database.java
@@ -15,8 +15,11 @@ package dbase;
  */
 public class Database {
     
+	private SystemCatalog catalog;
+	
     /** Creates a new instance of Database. */
     public Database() {
+    	catalog = new SystemCatalog();
     }
     
     /**This will create a new database if one isn't specified by the user.
@@ -32,18 +35,27 @@ public class Database {
      */
     public boolean parseCommand(final String command) {
     	//Split the command into an array of strings
-    	String [] splitCommand = command.split("/\\s+/");
+    	String [] splitCommand = command.split(" ");
     	
     	//TODO send the result to SystemCatalog
     	if (splitCommand[0].compareToIgnoreCase("SELECT") == 1) {
     		
-    	} else if (splitCommand[0].compareToIgnoreCase("SELECT") == 1) {
+    	} else if (splitCommand[0].compareToIgnoreCase("CREATE") == 1) {
+    		if (splitCommand[1].compareToIgnoreCase("TABLE") == 1){
+    			return catalog.createTable(command, "key");
+    		} else if (splitCommand[1].compareToIgnoreCase("INDEX") == 1){
+    			//TODO: Parse command and pass to createIndex()
+    			//catalog.createIndex(command, "key");
+    			return true;
+    		} else {
+    			return false;
+    		}
     		
     	} else if (splitCommand[0].compareToIgnoreCase("INSERT") == 1) {
     		
     	}
     	
-        return true;
+        return false;
     }
     
     /**The main function for the whole dealie.
