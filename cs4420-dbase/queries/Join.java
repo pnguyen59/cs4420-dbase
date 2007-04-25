2deaa14472189c84f1a56298fe4ec6addee04192
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-25 16:44:34 +0000

    Implements TreeNode now.

--- cs4420-dbase/queries/Join.java
+++ cs4420-dbase/queries/Join.java
@@ -1,18 +1,44 @@
 package queries;
-import java.util.ArrayList;
 
 public class Join extends Operation {
 
-	protected ArrayList < Condition > conditions;
-	
-	protected int tableTwoID;
+	protected Condition conditions;
 	
 	protected int tableTwoAccess;
+	
+	protected int tableTwoID;
 	
 	@Override
 	public long calculateCost() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	/**This method will return whether or not the Join allows children as
+	 * per the <code>TreeNode</code> interface.
+	 * @return <code><b>true</b></code> becase a Join must always 
+	 * have children.
+	 */
+	public boolean getAllowsChildren() {
+		return true;
+	}
+	
+	/**This method will return the number of children that this Join has
+	 * as per the TreeNode interface.
+	 * @return The number of children of this node.
+	 */
+	public int getChildCount() {
+		//TODO fix this so it does something.
+		return 0;
+	}
+	
+	/**Says whether or not the Join is a Leaf.  
+	 * Always false, a Join is never a Leaf in a query tree.
+	 * @return <code><b>false</b></code> because Join statements
+	 * aren't leaves.
+	 */
+	public boolean isLeaf() {
+		return false;
 	}
 	
 	public String toString() {
@@ -22,10 +48,6 @@ public class Join extends Operation {
 		string += this.queryID + "\t";
 		string += this.executionOrder + "\t";
 		string += this.type + "\t";
-		for (int index = 0; index < conditions.size(); index++) {
-			string += conditions.get(index).toString() + ", ";
-		}
-		string += tableOneID + "\t";
 		string += tableTwoID + "\t";
 		string += tableOneAccess +"\t";
 		string += tableTwoAccess +"\t";
