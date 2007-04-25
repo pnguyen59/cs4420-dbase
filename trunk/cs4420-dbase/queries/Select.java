2b90600fd024f17a7f98381fa2f0afda3b4aac23
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-25 16:39:15 +0000

    Implements TreeNode now.

--- trunk/cs4420-dbase/queries/Select.java
+++ trunk/cs4420-dbase/queries/Select.java
@@ -1,17 +1,65 @@
 package queries;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.concurrent.locks.Condition;
+
+import javax.swing.tree.TreeNode;
 
 
 public class Select extends Operation {
 
-	protected ArrayList < Condition > conditions;
+	protected Condition condition;
+	
+	/**This will create a new instance of Select.  It will use the 
+	 * statement passed to it to find out what the condition of the select is
+	 * and what it is selecting from.
+	 * @param selectStatement The definition of the select.
+	 */
+	public Select (final String selectStatement) {
+		
+		//Tet the list of the parts of this select
+		ArrayList < String > selectionParts = 
+			QueryParser.parseStatementParts(selectStatement);
+		
+		//Find out what this thing is selecting from
+		
+		
+		//Find the condition from the select.
+		
+		
+	}
 	
 	@Override
 	public long calculateCost() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	/**This method will return whether or not the Select allows children as
+	 * per the <code>TreeNode</code> interface
+	 * @return <code><b>true</b></code> becase a Select must always 
+	 * have children.
+	 */
+	public boolean getAllowsChildren() {
+		return true;
+	}
+	
+	/**This method will return the number of children that this Select has
+	 * as per the TreeNode interface.
+	 * @return The number of children of this node.
+	 */
+	public int getChildCount() {
+		//TODO fix this so it does something.
+		return 0;
+	}
+	
+	/**Says whether or not the Selection is a Leaf.  Always false, a Select is 
+	 * never a Leaf in a query tree.
+	 * @return <code<b>false</b></code> because select statements aren't leaves.
+	 */
+	public boolean isLeaf() {
+		return false;
 	}
 	
 	public String toString() {
@@ -21,15 +69,11 @@ public class Select extends Operation {
 		string += this.queryID + "\t";
 		string += this.executionOrder + "\t";
 		string += this.type + "\t";
-		for (int index = 0; index < conditions.size(); index++) {
-			string += conditions.toString() + ", ";
-		}
-		string += tableOneID + "\t\t";
+		string += condition.toString();
 		string += tableOneAccess +"\t\t";
 		string += "\t";
 		string += resultTableID;
 		
 		return string;
-	}	
-
+	}
 }
