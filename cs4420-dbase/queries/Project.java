d55cf6f0e68314e7de08e43bbf848e02c9525b64
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-25 16:43:10 +0000

    Implements TreeNode now.

--- cs4420-dbase/queries/Project.java
+++ cs4420-dbase/queries/Project.java
@@ -22,12 +22,38 @@ public class Project extends Operation {
 		for (int index = 0; index < attributes.size(); index++) {
 			string += attributes.get(index).getName() + ", ";
 		}
-		string += tableOneID + "\t\t";
 		string += tableOneAccess +"\t\t";
 		string += "\t";
 		string += resultTableID;
 		
 		return string;
+	}
+	
+	/**This method will return whether or not the Project allows children as
+	 * per the <code>TreeNode</code> interface.
+	 * @return <code><b>true</b></code> becase a Project must always 
+	 * have children.
+	 */
+	public boolean getAllowsChildren() {
+		return true;
+	}
+	
+	/**This method will return the number of children that this Project has
+	 * as per the TreeNode interface.
+	 * @return The number of children of this node.
+	 */
+	public int getChildCount() {
+		//TODO fix this so it does something.
+		return 0;
+	}
+	
+	/**Says whether or not the Projection is a Leaf.  
+	 * Always false, a Project is never a Leaf in a query tree.
+	 * @return <code><b>false</b></code> because Project statements
+	 * aren't leaves.
+	 */
+	public boolean isLeaf() {
+		return false;
 	}
 	
 }
