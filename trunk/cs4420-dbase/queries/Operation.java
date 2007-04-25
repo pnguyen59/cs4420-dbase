86fa6d0174e4fe5e892202ba9fdede7100a1882d
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-25 16:53:30 +0000

    Added getters and setters.

--- trunk/cs4420-dbase/queries/Operation.java
+++ trunk/cs4420-dbase/queries/Operation.java
@@ -34,47 +34,22 @@ public abstract class Operation implements TreeNode  {
 		return null;
 	}
 	
-	protected String type;
+	protected int executionOrder;
+	
+	protected Operation parent;
 	
 	protected int queryID;
 	
-	protected int executionOrder;
+	protected int resultTableID;
+	
+	/**The Operation from whence this one gets its data, a child.*/
+	protected Operation tableOne;
 	
 	protected int tableOneAccess;
 	
-	protected int resultTableID;
-	
-	protected Operation parent;
+	protected String type;
 
-	public void setType(final String newType) {
-		this.type = newType;
-	}
-	
-	
-	/**This returns the parent of this Select, probably another operation, but
-	 * return it as a <code>TreeNode</code> because of the interface.
-	 * @return The Operation that owns this one.
-	 */
-	public TreeNode getParent() {
-		return this.parent;
-	}
-	
-	/**The index of a Child.  Don't know how that works.
-	 * @child The child to find the index of
-	 * @return nothing.
-	 */
-	public int getIndex(final TreeNode child) {
-		return 0;
-	}
-	
-	
-	/**Gets a child at an index.  Don't know how that works.
-	 * @param index Index of the child.
-	 * @return nothing.
-	 */
-	public TreeNode getChildAt(final int index) {
-		return null;
-	}	
+	public abstract long calculateCost();
 	
 	/**This does something I don't understand.
 	 * @return Stuff.
@@ -82,6 +57,130 @@ public abstract class Operation implements TreeNode  {
 	public Enumeration children() {
 		return null;
 	}
+	
+	/**Gets a child at an index.  Don't know how that works.
+	 * @param index Index of the child.
+	 * @return nothing.
+	 */
+	public TreeNode getChildAt(final int index) {
+		return null;
+	}
+	
+	
+	/**This method returns the value of executionOrder.
+	 * @return the executionOrder
+	 */
+	public int getExecutionOrder() {
+		return executionOrder;
+	}	
+	
+	/**The index of a Child.  Don't know how that works.
+	 * @param child The child to find the index of
+	 * @return nothing.
+	 */
+	public int getIndex(final TreeNode child) {
+		return 0;
+	}
 
-	public abstract long calculateCost();
+	/**This returns the parent of this Select, probably another operation, but
+	 * return it as a <code>TreeNode</code> because of the interface.
+	 * @return The Operation that owns this one.
+	 */
+	public TreeNode getParent() {
+		return this.parent;
+	}
+
+
+	/**This method returns the value of queryID.
+	 * @return the queryID
+	 */
+	public int getQueryID() {
+		return queryID;
+	}
+
+
+	/**This method returns the value of resultTableID.
+	 * @return the resultTableID
+	 */
+	public int getResultTableID() {
+		return resultTableID;
+	}
+
+
+	/**This method returns the value of tableOne.
+	 * @return the tableOne
+	 */
+	public Operation getTableOne() {
+		return tableOne;
+	}
+
+
+	/**This method returns the value of tableOneAccess.
+	 * @return the tableOneAccess
+	 */
+	public int getTableOneAccess() {
+		return tableOneAccess;
+	}
+
+
+	/**This method returns the value of type.
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+
+	/**This method will set the value of executionOrder.
+	 * @param newExecutionOrder The new value of executionOrder.
+	 */
+	public void setExecutionOrder(final int newExecutionOrder) {
+		this.executionOrder = newExecutionOrder;
+	}
+
+
+	/**This method will set the value of parent.
+	 * @param newParent The new value of parent.
+	 */
+	public void setParent(final Operation newParent) {
+		this.parent = newParent;
+	}
+
+
+	/**This method will set the value of queryID.
+	 * @param newQueryID The new value of queryID.
+	 */
+	public void setQueryID(final int newQueryID) {
+		this.queryID = newQueryID;
+	}
+
+
+	/**This method will set the value of resultTableID.
+	 * @param newResultTableID The new value of resultTableID.
+	 */
+	public void setResultTableID(final int newResultTableID) {
+		this.resultTableID = newResultTableID;
+	}
+
+
+	/**This method will set the value of tableOne.
+	 * @param newTableOne The new value of tableOne.
+	 */
+	public void setTableOne(final Operation newTableOne) {
+		this.tableOne = newTableOne;
+	}
+
+
+	/**This method will set the value of tableOneAccess.
+	 * @param newTableOneAccess The new value of tableOneAccess.
+	 */
+	public void setTableOneAccess(final int newTableOneAccess) {
+		this.tableOneAccess = newTableOneAccess;
+	}
+
+
+	public void setType(final String newType) {
+		this.type = newType;
+	}
+	
 }
