61f3d5d119e165ab5b5a8122bc4386bcf06c91cf
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-25 17:56:19 +0000

    Really just a wrapper for tables so they can be in the tree.

--- cs4420-dbase/queries/TableOperation.java
+++ cs4420-dbase/queries/TableOperation.java
@@ -23,9 +23,23 @@ public class TableOperation extends Operation {
 		return 0;
 	}
 
+	/**This method returns the value of tableName.
+	 * @return the tableName
+	 */
+	public String getTableName() {
+		return tableName;
+	}
+
 	public boolean isLeaf() {
 		// TODO Auto-generated method stub
 		return true;
+	}
+
+	/**This method will set the value of tableName.
+	 * @param newTableName The new value of tableName.
+	 */
+	public void setTableName(final String newTableName) {
+		this.tableName = newTableName;
 	}
 
 }
