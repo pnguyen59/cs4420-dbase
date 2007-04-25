3dd0909caf25aa68ec0d3ece77f58c852e391149
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-25 16:45:48 +0000

    Implements TreeNode now.

--- cs4420-dbase/queries/CrossJoin.java
+++ cs4420-dbase/queries/CrossJoin.java
@@ -2,14 +2,41 @@ package queries;
 
 public class CrossJoin extends Operation {
 
-	protected int tableTwoID;
-	
 	protected int tableTwoAccess;
+	
+	protected int tableTwoID;
 	
 	@Override
 	public long calculateCost() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	/**This method will return whether or not the CrossJoin allows children as
+	 * per the <code>TreeNode</code> interface.
+	 * @return <code><b>true</b></code> becase a CrossJoin must always 
+	 * have children.
+	 */
+	public boolean getAllowsChildren() {
+		return true;
+	}
+	
+	/**This method will return the number of children that this CrossJoin has
+	 * as per the TreeNode interface.
+	 * @return The number of children of this node.
+	 */
+	public int getChildCount() {
+		//TODO fix this so it does something.
+		return 0;
+	}
+	
+	/**Says whether or not the CrossJoin is a Leaf.  
+	 * Always false, a CrossJoin is never a Leaf in a query tree.
+	 * @return <code><b>false</b></code> because CrossJoin statements
+	 * aren't leaves.
+	 */
+	public boolean isLeaf() {
+		return false;
 	}
 	
 	public String toString() {
@@ -20,7 +47,6 @@ public class CrossJoin extends Operation {
 		string += this.executionOrder + "\t";
 		string += this.type + "\t";
 		string += "\t";
-		string += tableOneID + "\t";
 		string += tableTwoID + "\t";
 		string += tableOneAccess +"\t";
 		string += tableTwoAccess +"\t";
