86a771af5b05e3fe5e28f168ae3f72a4aaf1e7d0
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-26 22:45:07 +0000

    Stubbage

--- cs4420-dbase/queries/Query.java
+++ cs4420-dbase/queries/Query.java
@@ -1,5 +1,7 @@
 package queries;
 
+import java.util.ArrayList;
+
 public class Query {
 
 	private Operation treeRoot;
@@ -24,6 +26,15 @@ public class Query {
 		this.treeRoot = newTreeRoot;
 	}
 	
+	public ArrayList < String > getRelations() {
+		
+		return null;
+	}
+	
+	public ArrayList < String > getAttributes() {
+		
+		return null;
+	}
 	
 	
 	
