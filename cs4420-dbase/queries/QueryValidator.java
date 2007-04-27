60979c26b21657833e212a92c1dfcbae78d31dca
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-27 17:44:38 +0000

    Fixed a thing in validate attributes that made it so the attribute had to be in the first table of the query.  Not it can be anywhere.

--- cs4420-dbase/queries/QueryValidator.java
+++ cs4420-dbase/queries/QueryValidator.java
@@ -4,40 +4,10 @@ import java.util.ArrayList;
 
 import dbase.Relation;
 import dbase.RelationHolder;
+import dbase.Attribute.Type;
 
 public class QueryValidator {
 
-	
-	public static boolean validateQuery(final Query query) {
-		
-		//See if the tables and attributes are valid
-		if (!validateTables(query)) {
-			return false;
-		} else if (!validateAttributes(query)) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	public static boolean validateTables(final Query query) {
-		
-		
-		ArrayList < String > tables = query.getRelations();
-		
-		//For the list of tables, see that they exist
-		RelationHolder holder = RelationHolder.getRelationHolder();
-		
-		for (int index = 0; index < tables.size(); index++) {
-			int relationID = holder.getRelationByName(tables.get(index));
-			Relation relation = holder.getRelation(relationID);
-			if (relation == null) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
 	
 	public static boolean validateAttributes(final Query query) {
 		
@@ -62,7 +32,7 @@ public class QueryValidator {
 			
 			ArrayList < Relation > containing = new ArrayList < Relation > ();
 			
-			//Go through all of the tables to see which, if any containt the
+			//Go through all of the tables to see which, if any contain the
 			//attribute
 			for (int inner = 0; inner < tables.size(); inner++) {
 				
@@ -71,19 +41,17 @@ public class QueryValidator {
 					containing.add(relation);
 				}
 				
-				//See if more than one relation has this attribute
-				if (containing.size() > 1) { 
-					System.out.println("More than one relation has " 
-						+ attributeName);
-					return false;
-				} else if (containing.size() == 0) {
-					System.out.println("No relation has "
-						+ attributeName);
-					return false;
-				}
-				
 			}
-			
+			//See if more than one relation has this attribute
+			if (containing.size() > 1) { 
+				System.out.println("More than one relation has " 
+					+ attributeName);
+				return false;
+			} else if (containing.size() == 0) {
+				System.out.println("No relation has "
+					+ attributeName);
+				return false;
+			}
 		}
 		
 		return true;
@@ -91,7 +59,7 @@ public class QueryValidator {
 	
 	public static boolean validateConditions(final Query query) {
 		
-		ArrayList < Condition > comparisons = query.getConditions();
+		ArrayList < SimpleCondition > conditions = query.getConditions();
 		
 		//Get the list of table IDx
 		ArrayList < String > tableIDs = query.getRelations();
@@ -106,8 +74,65 @@ public class QueryValidator {
 			int relationID = holder.getRelationByName(tableIDs.get(index));
 			tables.add(holder.getRelation(relationID));
 		}	
+		
+		//Go throug the list of conditions and make sure that all the 
+		//sides match up
+		for (int index = 0; index < conditions.size(); index++) {
 			
-		return false;	
+			//Get the current condition
+			SimpleCondition currentCondition = conditions.get(index);
+			
+			//Get the rightHand and leftHAnd
+			String leftHand = currentCondition.getLeftHand();
+			String rightHand = currentCondition.getRightHand();
+			
+			//Find the type for each
+			Type leftHandType =
+				Utilities.findAttributeType(leftHand, tables);
+			Type rightHandType =
+				Utilities.findAttributeType(rightHand, tables);
+			
+			if (leftHandType != rightHandType) {
+				System.out.println("Attributes are of different type.");
+				return false;
+			}
+		}
+			
+		return true;
+	
+	}
+	
+	public static boolean validateQuery(final Query query) {
+		
+		//See if the tables and attributes are valid
+		if (!validateTables(query)) {
+			return false;
+		} else if (!validateAttributes(query)) {
+			return false;
+		} else if (!validateConditions(query)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public static boolean validateTables(final Query query) {
+		
+		
+		ArrayList < String > tables = query.getRelations();
+		
+		//For the list of tables, see that they exist
+		RelationHolder holder = RelationHolder.getRelationHolder();
+		
+		for (int index = 0; index < tables.size(); index++) {
+			int relationID = holder.getRelationByName(tables.get(index));
+			Relation relation = holder.getRelation(relationID);
+			if (relation == null) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 	
 	
