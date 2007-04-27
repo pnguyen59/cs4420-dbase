fa7cbea736f883f2f3cc0a4408309e36f5324ea5
killer.ego <killer.ego@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-27 18:23:17 +0000

    added some code in selct necessary for optimization

--- cs4420-dbase/queries/QueryOptimizer.java
+++ cs4420-dbase/queries/QueryOptimizer.java
@@ -7,38 +7,37 @@ import java.util.ArrayList;
 
 /**
  * @author gtg471h
- *
+ * 
  */
 public class QueryOptimizer {
-	
+
 	/**
-	 * Optimizes the current tree when handed the head of the tree. 
-	 * If the node given isn't the head it finds the head and starts from that point.
-	 * @param head The head of the Query tree.
+	 * Optimizes the current tree when handed the head of the tree. If the node
+	 * given isn't the head it finds the head and starts from that point.
+	 * 
+	 * @param head
+	 *            The head of the Query tree.
 	 */
-	
+
 	public static void OptimizeTree(Operation head)
 	{		
 		Operation parent, child1, child2;
 		if (head.getType().equals(QueryParser.PROJECT))
 		{
 			OptimizeTree((Project)head.getTableOne());
-			//TODO implement projection push here.
+			// TODO implement projection push here.
 		} 
 		else if (head.getType().equals(QueryParser.SELECT))
 		{
 			child1 = head.getTableOne();
-			child1.setParent((Operation) head.getParent());
 			
-
-			head.setParent(child1);
-			child1.getTableOne().setParent(head);
 			if (child1.getType().equals(QueryParser.JOIN) || child1.getType().equals(QueryParser.JOIN))
 			{
 				ArrayList<Operation> left = new ArrayList <Operation> (), right = new ArrayList <Operation> ();
 				left.add(child1.getTableOne());
 				right.add(child1.getTableTwo());
 				Select leftsel, rightsel;
+				leftsel = (Select) Operation.makeOperation("Select");
 				boolean finishedleft = true, finishedright = true;
 				while (finishedleft)
 				{
@@ -81,29 +80,61 @@ public class QueryOptimizer {
 					}
 				}
 				
+				Condition cond = ((Select) head).getCondition();
+				
+				if (cond.comparison.equals(QueryParser.OR))
+				{
+					OptimizeTree(head.getTableOne());
+					return;
+				}
+				boolean cont;
+						
+				cont = (condtraverse(((AndOrCondition) cond).getLeftHand()) && condtraverse(((AndOrCondition) cond).getRightHand()));
+				
+				if (!cont)
+				{
+					OptimizeTree(head.getTableOne());
+					return;
+				}
+				
 				ArrayList <String> alist = head.getAttributes();
 				for (int i = 0; i < alist.size(); i++)
 				{
+					child1.setParent((Operation) head.getParent());
 					
+					head.setParent(child1);
+					child1.getTableOne().setParent(head);
 				}
 			}
 		}
 		else if (head.getType().equals(QueryParser.CROSSJOIN))
 		{
-			//TODO choose order of joins
+			// TODO choose order of joins
 		}
 		else if (head.getType().equals(QueryParser.JOIN))
 		{
-			//TODO choose order of joins
+			// TODO choose order of joins
 		}
 		else if (head.getType().equals(QueryParser.TABLEOPERATION))
 		{
 			return;
 		}
 	}
-	
-	public static void OptimizeProjects(Operation root, ArrayList<String> attrs){
-		
+
+	private static boolean condtraverse(Condition cond) {
+		if (cond.condition.equals(QueryParser.AND))
+		{
+			return (condtraverse(((AndOrCondition) cond).getLeftHand()) && condtraverse(((AndOrCondition) cond).getRightHand()));
+		} 
+		else if (cond.condition.equals(QueryParser.OR))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	public static void OptimizeProjects(Operation root, ArrayList<String> attrs) {
+
 	}
 
 }
