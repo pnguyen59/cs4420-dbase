624a69629304206bd0b27d9da59a73cf3da8673d
Euglenas <Euglenas@87fe2c97-c82a-0410-b554-e13d1092c3cc>
2007-04-27 17:46:16 +0000

    Made a method to find the attribute from a statement.

--- trunk/cs4420-dbase/queries/Utilities.java
+++ trunk/cs4420-dbase/queries/Utilities.java
@@ -1,5 +1,11 @@
 package queries;
 
+import java.util.ArrayList;
+
+import dbase.Attribute;
+import dbase.Relation;
+import dbase.Attribute.Type;
+
 /**
  * This class is for stuff used anywhere that doesn't really have a proper home
  * @author dkitch
@@ -34,5 +40,48 @@ public class Utilities {
 		} else {
 			return vals[0];
 		}
+	
+	
 	}
+	
+	public static Type findAttributeType(final String attributeStatement,
+		final ArrayList < Relation > relations) {
+		
+		//Trim and chop off the parens
+		String noParens = attributeStatement.replace(")", "");
+		noParens = noParens.replace("(", "");
+		String trimmed = noParens.trim();
+		
+		//First see if it is a constant, if so then horray, much easier
+		if (trimmed.substring(0,1).equalsIgnoreCase("K")) {
+			//Find the next real letter, to determine what type it is
+			trimmed = trimmed.replace(" ", "");
+			String type = trimmed.substring(1, 2);
+			if (type.equalsIgnoreCase("I")) {
+				return Type.Int;
+			} else if (type.equalsIgnoreCase("C")) {
+				return Type.Char;
+			} else if (type.equalsIgnoreCase("D")) {
+				return Type.DateTime;
+			}
+		}
+		
+		//Since it is now not a constant if it is here, we know it is an
+		//attribute.  Find the relation that has it and return the type.
+		String attributeName = QueryParser.parseAttribute(attributeStatement);
+		for (int index = 0; index < relations.size(); index++) {
+		
+			//Get the relation out and see if it contains the attribute
+			Relation currentRelation = relations.get(index);
+			Attribute attribute = 
+				currentRelation.getAttributeByName(attributeName);
+			
+			//If it isn't null then return the type
+			if (attribute != null) {
+				return attribute.getType();
+			}
+		}
+		
+		return null;
 	}
+}
